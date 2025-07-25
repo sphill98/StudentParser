5d2a013dbd4c10920d14a65443544670390db1cf
sphill98 <sphill9803@gmail.com>
2025-07-25 16:48:37 +0900

    FEATURED : Gunicorn update

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from student_parser import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=7104)
+    # app.config에서 PORT 값을 가져와 사용
+    app.run(host='0.0.0.0', debug=True, port=app.config['PORT'])
